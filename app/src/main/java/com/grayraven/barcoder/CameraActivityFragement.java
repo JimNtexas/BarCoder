30b911bf5e37726752bdd65d96f80375e9a8f5ae
Jim Howard <jhoward@affinegy.com>
2015-09-22 17:29:34 -0500

    preview and capture working

--- app/src/main/java/com/grayraven/barcoder/CameraActivityFragement.java
+++ app/src/main/java/com/grayraven/barcoder/CameraActivityFragement.java
@@ -15,20 +15,28 @@ package com.grayraven.barcoder;
  * limitations under the License.
  */
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Camera;
-import android.media.Image;
 import android.os.Bundle;
-import android.os.Handler;
+import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class CameraActivityFragement extends android.support.v4.app.Fragment
@@ -42,6 +50,10 @@ public class CameraActivityFragement extends android.support.v4.app.Fragment
     List<Camera.Size> mSupportedPreviewSizes;
     SurfaceHolder mHolder;
     SurfaceView mSurfaceView;
+    int mCameraId = 0;
+    Camera.PictureCallback rawCallback;
+    Camera.ShutterCallback shutterCallback;
+    Camera.PictureCallback jpegCallback;
 
 
     public static CameraActivityFragement newInstance() {
@@ -57,16 +69,22 @@ public class CameraActivityFragement extends android.support.v4.app.Fragment
 
      @Override
       public void onViewCreated(final View view, Bundle savedInstanceState) {
-  //        view.findViewById(R.id.picture).setOnClickListener(this);
-    //      view.findViewById(R.id.info).setOnClickListener(this);
          mSurfaceView = (SurfaceView) view.findViewById(R.id.surface_view);
 
-       /*  rawCallback = new PictureCallback() {
-             public void onPictureTaken(byte[] data, Camera camera) {
-                 Log.d("Log", "onPictureTaken - raw");
-             }
-         };*/
+       Button captureButton = (Button)view.findViewById(R.id.capture_image);
+       captureButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               CaptureImage();
+
+           }
+       });
      }
+
+    private void CaptureImage() {
+        mCamera.takePicture(shutterCallback, rawCallback, jpegCallback);
+
+    }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -80,26 +98,15 @@ public class CameraActivityFragement extends android.support.v4.app.Fragment
         super.onResume();
         Log.d(TAG, "onResume");
         setCamera(mCamera);
-
-        //startBackgroundThread();
-
-        // When the screen is turned off and turned back on, the SurfaceTexture is already
-        // available, and "onSurfaceTextureAvailable" will not be called. In that case, we can open
-        // a camera and start preview from here (otherwise, we wait until the surface is ready in
-        // the SurfaceTextureListener).
-      /*  if (mTextureView.isAvailable()) {
-            openCamera(mTextureView.getWidth(), mTextureView.getHeight());
-        } else {
-            mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
-        }*/
     }
 
     @Override
     public void onPause() {
         stopPreviewAndFreeCamera();
-        Log.d(TAG, "onPause");
+        Log.d(TAG, "capture fragement onPause");
         super.onPause();
     }
+
 
     public void setCamera(Camera camera) {
         stopPreviewAndFreeCamera();
@@ -107,19 +114,22 @@ public class CameraActivityFragement extends android.support.v4.app.Fragment
         mHolder = mSurfaceView.getHolder();
         mHolder.addCallback(this);
         mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-
-
         safeCameraOpen();
         if (mCamera != null) {
-            List<Camera.Size> localSizes = mCamera.getParameters().getSupportedPreviewSizes();
-            mSupportedPreviewSizes = localSizes;
-           // mPreview = new Preview(mContext);
+           // List<Camera.Size> localSizes = mCamera.getParameters().getSupportedPreviewSizes();
+           // mSupportedPreviewSizes = localSizes;
+
+            Camera.Parameters params = mCamera.getParameters();
+            params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+
             try {
                 mCamera.setPreviewDisplay(mHolder);
             } catch (IOException e) {
                 Log.e(TAG, "could not set preview display");
                 return;
             }
+
+            SetupCallbacks();
             mCamera.startPreview();
         }
     }
@@ -138,6 +148,48 @@ public class CameraActivityFragement extends android.support.v4.app.Fragment
     }
 
 
+    private void SetupCallbacks() {
+        rawCallback = new Camera.PictureCallback() {
+            public void onPictureTaken(byte[] data, Camera camera) {
+                Log.d("Log", "onPictureTaken - raw");
+            }
+        };
+
+        /** Handles data for jpeg picture */
+        shutterCallback = new Camera.ShutterCallback() {
+            public void onShutter() {
+                Log.i("Log", "onShutter'd");
+            }
+        };
+
+        jpegCallback = new Camera.PictureCallback() {
+            public void onPictureTaken(byte[] data, Camera camera) {
+                File pictureFile = getOutputMediaFile();
+                if (pictureFile == null) {
+                    Toast.makeText(getActivity(), "Image retrieval failed.", Toast.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+
+                try {
+                    FileOutputStream fos = new FileOutputStream(pictureFile);
+                    fos.write(data);
+                    fos.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                /***
+                 * todo:  check for barcode, if found, go back to main, if not give user another change
+                 */
+                Intent i=new Intent(getActivity(),MainActivity.class);
+                startActivity(i);
+            }
+        };
+    }
+
 
     private boolean safeCameraOpen() {
         boolean qOpened = false;
@@ -152,6 +204,7 @@ public class CameraActivityFragement extends android.support.v4.app.Fragment
                 Camera.getCameraInfo(i, cameraInfo);
                 if(cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
                    mCamera = Camera.open(i);
+                   mCameraId = i;
                    break;
                 }
             }
@@ -174,38 +227,17 @@ public class CameraActivityFragement extends android.support.v4.app.Fragment
         }
     }
 
-    /**
-     * Starts a background thread and its {@link Handler}.
-     */
-    private void startBackgroundThread() {
-     /*   mBackgroundThread = new HandlerThread("CameraBackground");
-        mBackgroundThread.start();
-        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());*/
-    }
-
-    /**
-     * Stops the background thread and its {@link Handler}.
-     */
-    private void stopBackgroundThread() {
-       /* mBackgroundThread.quitSafely();
-        try {
-            mBackgroundThread.join();
-            mBackgroundThread = null;
-            mBackgroundHandler = null;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }*/
-    }
-
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         Log.d(TAG, "sufaceCreated");
         startCameraPreview();
+        setCameraDisplayOrientation(getActivity(), mCameraId, mCamera);
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
         Log.d(TAG, "sufaceChanged");
+        Log.d(TAG, "surface size (WxH):" + width + " - " + height);
     }
 
     @Override
@@ -214,199 +246,51 @@ public class CameraActivityFragement extends android.support.v4.app.Fragment
 
     }
 
-
-    /**
-     * Configures the necessary {@link android.graphics.Matrix} transformation to `mTextureView`.
-     * This method should be called after the camera preview size is determined in
-     * setUpCameraOutputs and also the size of `mTextureView` is fixed.
-     *
-     * @param viewWidth  The width of `mTextureView`
-     * @param viewHeight The height of `mTextureView`
-     */
-    private void configureTransform(int viewWidth, int viewHeight) {
-     /*   Activity activity = getActivity();
-        if (null == mTextureView || null == mPreviewSize || null == activity) {
-            return;
+    public static void setCameraDisplayOrientation(Activity activity,
+                                                   int cameraId, android.hardware.Camera camera) {
+        android.hardware.Camera.CameraInfo info =
+                new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(cameraId, info);
+        int rotation = activity.getWindowManager().getDefaultDisplay()
+                .getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0: degrees = 0; break;
+            case Surface.ROTATION_90: degrees = 90; break;
+            case Surface.ROTATION_180: degrees = 180; break;
+            case Surface.ROTATION_270: degrees = 270; break;
         }
-        int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
-        Matrix matrix = new Matrix();
-        RectF viewRect = new RectF(0, 0, viewWidth, viewHeight);
-        RectF bufferRect = new RectF(0, 0, mPreviewSize.getHeight(), mPreviewSize.getWidth());
-        float centerX = viewRect.centerX();
-        float centerY = viewRect.centerY();
-        if (Surface.ROTATION_90 == rotation || Surface.ROTATION_270 == rotation) {
-            bufferRect.offset(centerX - bufferRect.centerX(), centerY - bufferRect.centerY());
-            matrix.setRectToRect(viewRect, bufferRect, Matrix.ScaleToFit.FILL);
-            float scale = Math.max(
-                    (float) viewHeight / mPreviewSize.getHeight(),
-                    (float) viewWidth / mPreviewSize.getWidth());
-            matrix.postScale(scale, scale, centerX, centerY);
-            matrix.postRotate(90 * (rotation - 2), centerX, centerY);
-        } else if (Surface.ROTATION_180 == rotation) {
-            matrix.postRotate(180, centerX, centerY);
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
         }
-        mTextureView.setTransform(matrix);*/
-    }
-
-    /**
-     * Begin the preview of the camera input.
-     */
-
-
-    /**
-     * Initiate a still image capture.
-     */
-    private void takePicture() {
-        lockFocus();
-    }
-
-    /**
-     * Lock the focus as the first step for a still image capture.
-     */
-    private void lockFocus() {
-     /*   try {
-            // This is how to tell the camera to lock focus.
-            mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AF_TRIGGER,
-                    CameraMetadata.CONTROL_AF_TRIGGER_START);
-            // Tell #mCaptureCallback to wait for the lock.
-            mState = STATE_WAITING_LOCK;
-            mCaptureSession.capture(mPreviewRequestBuilder.build(), mCaptureCallback,
-                    mBackgroundHandler);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }*/
-    }
-
-    private void runPrecaptureSequence() {
-      /*  try {
-            // This is how to tell the camera to trigger.
-            mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AE_PRECAPTURE_TRIGGER,
-                    CaptureRequest.CONTROL_AE_PRECAPTURE_TRIGGER_START);
-            // Tell #mCaptureCallback to wait for the precapture sequence to be set.
-            mState = STATE_WAITING_PRECAPTURE;
-            mCaptureSession.capture(mPreviewRequestBuilder.build(), mCaptureCallback,
-                    mBackgroundHandler);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }*/
+        camera.setDisplayOrientation(result);
     }
 
 
-    /**
-     * Unlock the focus. This method should be called when still image capture sequence is
-     * finished.
-     */
-    private void unlockFocus() {
-      /*  try {
-            // Reset the auto-focus trigger
-            mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AF_TRIGGER,
-                    CameraMetadata.CONTROL_AF_TRIGGER_CANCEL);
-            mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE,
-                    CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
-            mCaptureSession.capture(mPreviewRequestBuilder.build(), mCaptureCallback,
-                    mBackgroundHandler);
-            // After this, the camera will go back to the normal state of preview.
-            mState = STATE_PREVIEW;
-            mCaptureSession.setRepeatingRequest(mPreviewRequest, mCaptureCallback,
-                    mBackgroundHandler);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }*/
+    private File getOutputMediaFile(){
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "BarCoder");
+
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("Camera Guide", "Required media storage does not exist");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp + ".jpg");
+
+
+        return mediaFile;
     }
-
-/*    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.picture: {
-                takePicture();
-                break;
-            }
-            case R.id.info: {
-                Activity activity = getActivity();
-                if (null != activity) {
-                    new AlertDialog.Builder(activity)
-                            .setMessage(R.string.info_message)
-                            .setPositiveButton(android.R.string.ok, null)
-                            .show();
-                }
-                break;
-            }
-        }
-    }*/
-
-    /**
-     * Saves a JPEG {@link Image} into the specified {@link File}.
-     */
-  /* *//*     private static class ImageSaver implements Runnable {
-
-        *//**
-         * The JPEG image
-         *//*
-        private final Image mImage;
-        *//**
-         * The file we save the image into.
-         *//*
-        private final File mFile;
-
-        public ImageSaver(Image image, File file) {
-            mImage = image;
-            mFile = file;
-        }
-
-        @Override
-       public void run() {
-        ByteBuffer buffer = mImage.getPlanes()[0].getBuffer();
-            byte[] bytes = new byte[buffer.remaining()];
-            buffer.get(bytes);
-            FileOutputStream output = null;
-            try {
-                output = new FileOutputStream(mFile);
-                output.write(bytes);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                mImage.close();
-                if (null != output) {
-                    try {
-                        output.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }*//*
-
-
-
-        *//**
-         * Compares two {@code Size}s based on their areas.
-         *//*
-      *//*  static class CompareSizesByArea implements Comparator<Size> {
-
-            @Override
-            public int compare(Size lhs, Size rhs) {
-                // We cast here to ensure the multiplications won't overflow
-                return Long.signum((long) lhs.getWidth() * lhs.getHeight() -
-                        (long) rhs.getWidth() * rhs.getHeight());
-            }
-
-        }*//*
-
-        *//**
-         * Shows an error message dialog.
-         *//*
-        public static class ErrorDialog extends DialogFragment {
-
-            private static final String ARG_MESSAGE = "message";
-
-            public static ErrorDialog newInstance(String message) {
-                ErrorDialog dialog = new ErrorDialog();
-                Bundle args = new Bundle();
-                args.putString(ARG_MESSAGE, message);
-                dialog.setArguments(args);
-                return dialog;
-            }
-
-        }
-*/
 }
